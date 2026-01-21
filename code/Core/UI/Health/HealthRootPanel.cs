804d5312a32315fc8a91138549aab7b579c0b10e
Vic pro <stanleylaws1@gmail.com>
2026-01-21 11:21:11 +0100

    health hud arbo

---
+++ code/Core/UI/Health/HealthRootPanel.cs
@@ -0,0 +1,43 @@
+using Sandbox;
+using Sandbox.UI;
+
+namespace Astrofront;
+
+public sealed class HealthRootPanel : PanelComponent
+{
+    public Panel Root { get; private set; }
+
+    protected override void OnTreeFirstBuilt()
+    {
+        if ( Panel == null )
+        {
+            Log.Warning($"{nameof(HealthRootPanel)}: Panel is null.");
+            return;
+        }
+
+        if ( Root != null )
+        {
+            Root.Delete( true );
+            Root = null;
+        }
+
+        Root = new HealthBar();
+
+        Panel.AddClass( "fullscreen" );
+        Panel.AddChild( Root );
+    }
+
+    protected override void OnDisabled()
+    {
+        Root?.Delete( true );
+        Root = null;
+        base.OnDisabled();
+    }
+
+    protected override void OnDestroy()
+    {
+        Root?.Delete( true );
+        Root = null;
+        base.OnDestroy();
+    }
+}
